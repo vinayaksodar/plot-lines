c92d30eebbdf961e3fadcf60cf08f9c56e65cd7d
vinayaksodar <vinayakmsodar@gmail.com>
2025-10-25 15:59:05 +0000

    add github and discord links in sidemenu

--- apps/frontend/src/components/SideMenu/SideMenu.js
+++ apps/frontend/src/components/SideMenu/SideMenu.js
@@ -71,7 +71,8 @@ export function createSideMenu(
       svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
       </svg>`,
-      handler: () => window.open("https://github.com/your-repo", "_blank"),
+      handler: () =>
+        window.open("https://github.com/vinayaksodar/plot-lines", "_blank"),
     },
     {
       tooltip: "Join our Discord",
@@ -83,38 +84,38 @@ export function createSideMenu(
       <path d="M8.8 13a2 2 0 0 1 4 0"></path>
       <path d="M14.8 13a2 2 0 0 1 4 0"></path>
     </svg>`,
-      handler: () => window.open("https://discord.gg/9XpS2J4C", "_blank"),
+      handler: () => window.open("https://discord.gg/BDCR4SgSsU", "_blank"),
     },
-    {
-      tooltip: "Settings",
-      svg: `<svg xmlns="http://www.w3.org/2000/svg" 
-      width="24" height="24" viewBox="0 0 24 24" 
-      fill="none" stroke="currentColor" stroke-width="2" 
-      stroke-linecap="round" stroke-linejoin="round">
-      <circle cx="12" cy="12" r="3"></circle>
-      <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 
-        2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 
-        1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 
-        1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 
-        1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 
-        2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 
-        1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 
-        2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 
-        1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 
-        2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 
-        1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 
-        1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 
-        2 2v.09a1.65 1.65 0 0 0 1 
-        1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 
-        2 0 0 1 2.83 0 2 2 0 0 1 0 
-        2.83l-.06.06a1.65 1.65 0 0 0-.33 
-        1.82V9a1.65 1.65 0 0 0 
-        1.51 1H21a2 2 0 0 1 2 2 
-        2 2 0 0 1-2 2h-.09a1.65 
-        1.65 0 0 0-1.51 1z"></path>
-    </svg>`,
-      handler: () => console.log("Settings button clicked"),
-    },
+    // {
+    //   tooltip: "Settings",
+    //   svg: `<svg xmlns="http://www.w3.org/2000/svg"
+    //   width="24" height="24" viewBox="0 0 24 24"
+    //   fill="none" stroke="currentColor" stroke-width="2"
+    //   stroke-linecap="round" stroke-linejoin="round">
+    //   <circle cx="12" cy="12" r="3"></circle>
+    //   <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2
+    //     2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65
+    //     1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1
+    //     1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65
+    //     1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2
+    //     2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65
+    //     1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2
+    //     2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65
+    //     1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2
+    //     2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65
+    //     1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0
+    //     1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1
+    //     2 2v.09a1.65 1.65 0 0 0 1
+    //     1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2
+    //     2 0 0 1 2.83 0 2 2 0 0 1 0
+    //     2.83l-.06.06a1.65 1.65 0 0 0-.33
+    //     1.82V9a1.65 1.65 0 0 0
+    //     1.51 1H21a2 2 0 0 1 2 2
+    //     2 2 0 0 1-2 2h-.09a1.65
+    //     1.65 0 0 0-1.51 1z"></path>
+    // </svg>`,
+    //   handler: () => console.log("Settings button clicked"),
+    // },
   ];
 
   bottomButtons.forEach((config) => {
